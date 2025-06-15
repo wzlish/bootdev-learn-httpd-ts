f540042f1b53e1c2dd0e3ddb5c914614cfb6a7be
iamwzl <wzl@wzl.to>
2025-06-15 21:27:31 +0100

    CH3:L1 - Monoliths

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,12 @@ import { middlewareMetricsInc, middlewareLogResponses } from "./middleware.js";
 const app = express();
 const PORT = 8080;
 
-app.get("/healthz", handlerRediness);
+app.get("/api/healthz", handlerRediness);
 app.use("/", middlewareLogResponses);
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
-app.get("/metrics", handlerShowHits);
-app.get("/reset", handlerResetHits);
+app.get("/api/metrics", handlerShowHits);
+app.get("/api/reset", handlerResetHits);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
